f48400887da96681ffa6ef68766bbec3b4051d27
Xoni30 <101208057+Xoni30@users.noreply.github.com>
2024-02-22 15:42:11 +0000

    Update mobile layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col mt-8 mb-16`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-1">
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-1 mr-[15px]">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">10%</span> Discount For New Realtors.{" "}
@@ -14,9 +14,9 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] mr-15">
-            Marketing <br className="sm:block hidden" />{" "}
+        <div className="flex flex-row justify-between items-center w-full ">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] ">
+            Marketing <br/>{" "}
             <span className="text-gradient">Made Simple</span>{" "}
           </h1>
           
